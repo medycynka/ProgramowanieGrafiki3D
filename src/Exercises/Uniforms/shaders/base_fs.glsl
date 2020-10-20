bf8881f8312febd0eca0e8220889829bb375f50a
medycynka <tyrranozaur08@gmail.com>
2020-10-20 18:44:54 +0200

    fixed uniforms

--- src/Exercises/Uniforms/shaders/base_fs.glsl
+++ src/Exercises/Uniforms/shaders/base_fs.glsl
@@ -11,6 +11,4 @@ layout(std140) uniform Modifiers {
 void main() {
     vFragColor.a = vertex_color.a;
     vFragColor.rgb = light_intensity * light_color * vertex_color.rgb;
-    //vFragColor.xyz = vertex_color;
-//    vFragColor.w = 1.0f;
 }
